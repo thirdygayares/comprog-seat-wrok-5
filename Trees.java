d5b4c52adfe342085d9f7648d042e77a528853e5
thirdy454 <65775625+thirdy454@users.noreply.github.com>
2021-12-05 02:10:05 +0800

    First Fit Algorithm

--- Trees.java
+++ Trees.java
@@ -4,83 +4,66 @@ class Trees {
     static int processSize[] = new int[4];
     static int addressSize[] = new int[6];
     static int firstfitArray[] = new int[4];
-    static int haveStatusArray[] = new int[6];
+    static int haveStatusArray[] = new int[6]; //para di laktawan yung mayroon na 0 = default; 1=di na pwede icompare
     static Scanner sc = new Scanner(System.in);
     
     
   public static void main(String[] args) {
-	  for (int j=0;j<4;j++){
-		  
-	  }
-    
-    for (int sizeProcess =0;sizeProcess<4;sizeProcess++){
-    	
-        System.out.print("Please input the size of PROCESS" + " " + (sizeProcess+1) + ": ");
+	
+	  System.out.println("ENTER PROCESS SIZE");
+	  //inputing size process
+	  for (int sizeProcess =0;sizeProcess<4;sizeProcess++){
+        System.out.print("PROCESS " + (sizeProcess+1) + ": ");
         int storeProcessSizeInput = sc.nextInt();       
         processSize[sizeProcess] = storeProcessSizeInput; //storing a number of process size in array
        }
     
-    
-    //erase this later -->
-    for(int i = 0; i<4; i++)
-    {
-            System.out.print(processSize[i] + " ");
-        
-        System.out.println();
-    }     //<--erase this later 
-    
-    	System.out.println("---------------------------------------");
-    	System.out.println("---------------------------------------");
-  
+	  System.out.println();
+	  
+	  System.out.println("ENTER ADDRESS SIZE");
+	  //inputing size adress
 	    for(int sizeAdress = 0;sizeAdress<6;sizeAdress++){ 
 	    	
-	      System.out.print("Please input the size of ADDRESS" +  " "  + (sizeAdress + 1) + " ");
-	      int storeAddressSizeInput = sc.nextInt();  
+	      System.out.print("ADDRESS SIZE "  + (sizeAdress + 1) + ": ");
+	      	int storeAddressSizeInput = sc.nextInt();  
 	      	addressSize[sizeAdress] = storeAddressSizeInput; //storing a number of process size in array
-	      }
-	    
-	    //erase this later -->
-	    for(int i = 0; i<6; i++)
-	    {
-	      System.out.println(addressSize[i] + " ");
-	    } 
-	    
-	    System.out.println("---------------------------------------");
-    	System.out.println("---------------------------------------");
-  
-	    
-	    firstFit();
-	    
-	    //erase this later -->
-	    System.out.println("\nFirst Fit Algorithm");
-	    for(int ss = 0; ss<4; ss++)
-	    {
-	      System.out.println(firstfitArray[ss] + " ");
-	    } 
-	    
+	      }	  
+	    firstFit();  
   }
-  
-  	
-  
-  
-  
-  	static void firstFit() {
-  		 			 
-  		//transferring process array to haveStatus Array
-  		
-  		
-  		for(int x = 0; x<4; x++) {
-  			
-  			for(int j = 0; j<6; j++) {
-  				if(processSize[x] <= addressSize[j] && haveStatusArray[j] == 0) {
-  		  			firstfitArray[x] = addressSize[j];
-  		  			haveStatusArray[j]= 1;
-  		  			System.out.println("tetx: "  + processSize[x]);
-  		  		} 
-  			}
-  			
-  		}  	   
-  	}
-  
     
+  	static void firstFit() {	 		
+  		for(int x = 0; x<4; x++) {  
+  			if(processSize[x] <= addressSize[0] && haveStatusArray[0] == 0) {
+  	  			firstfitArray[x] = 1;
+  	  			haveStatusArray[0]= 1;
+  	  		} else if(processSize[x] <= addressSize[1] && haveStatusArray[1] == 0 ) {
+  	  			firstfitArray[x] = 2;
+  	  			haveStatusArray[1]= 1;
+  	  		}  else if(processSize[x] <= addressSize[2]  && haveStatusArray[2] == 0 ) {
+  	  			firstfitArray[x] = 3;
+  	  			haveStatusArray[2]= 1;
+  	  		}  else if(processSize[x] <= addressSize[3]  && haveStatusArray[3] == 0 ) {
+  	  			firstfitArray[x] = 4;
+  	  			haveStatusArray[3]= 1;
+  	  		} else if(processSize[x] <= addressSize[4]  && haveStatusArray[4] == 0 ) {
+  	  			firstfitArray[x] = 5;
+  	  			haveStatusArray[4]= 1;
+  	  		} else if(processSize[x] <= addressSize[5]  && haveStatusArray[5] == 0 ) {
+  	  			firstfitArray[x] = 6;
+  	  			haveStatusArray[5]= 1;
+  	  		}  			
+  			}	
+  		
+  		System.out.println("\nFirst Fit Algorithm\n");
+	    for(int ffAlgorithm = 0; ffAlgorithm<4; ffAlgorithm++)
+	    {
+	      System.out.println("PROCESS 1 TO ADDRESS SIZE" + firstfitArray[ffAlgorithm] );
+	    } 
+	    System.out.println();//spacing
+	    for(int vertical = 1; vertical<4; vertical++) {
+	    	 System.out.print("PROCESS " + vertical + " | ADDRESS " + firstfitArray[vertical] + "-> " );
+	    } System.out.print("PROCESS 4 | NULL ");
+	      		
+  	}
+  	  	
 }
